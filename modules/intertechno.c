7570a522a26985688811644943525b332c336018
Petre Rodan <petre.rodan@simplex.ro>
2012-11-06 19:15:07 +0200

    found cause of missing packet

--- modules/intertechno.c
+++ modules/intertechno.c
@@ -32,19 +32,20 @@
 // #       - enter config mode. use up, down, # to select family and device, * to save.
 //
 
-
 #include <openchronos.h>
 #include <drivers/display.h>
 #include <drivers/timer.h>
 #include <drivers/rf1a.h>
 
-#define INTERTECHNO_CMD_ON  0x7         // command for turning devices on
-#define INTERTECHNO_CMD_OFF 0x6         // command for turning devices off
-#define INTERTECHNO_CMD_SP  0xf         // special devices like doorbells, PIR detectors use this cmd
+#define INTERTECHNO_CMD_ON  0x07        // command for turning switches on
+#define INTERTECHNO_CMD_OFF 0x06        // command for turning switches off
+#define INTERTECHNO_CMD_SP  0x0f        // special devices like doorbells, PIR detectors use this cmd
+#define INTERTECHNO_SEQ_SIZE  16        // TX FIFO size to be allocated
+
+// starting values
 #define INTERTECHNO_DEF_FAMILY 12       // this translates as family 'L' on the rotary switch
 #define INTERTECHNO_DEF_DEVICE 7        // device number 7 on remotes that have devices numbered 1 to 16
-#define INTERTECHNO_SEQ_PKT   2         // how many packets to be sent with every sequence (1, 2 or 3)
-#define INTERTECHNO_SEQ_SIZE  INTERTECHNO_SEQ_PKT*16        // 16, 32 or 48 bytes to be allocated for the cmd sequence
+                                        // or device 3 group 2 on others
 
 #define st(x)                       do { x } while (__LINE__ == -1)
 #define ENTER_CRITICAL_SECTION(x)   st( x = __get_interrupt_state(); __disable_interrupt(); )
@@ -62,252 +63,220 @@ uint8_t tmp_device = INTERTECHNO_DEF_DEVICE;
 
 static void intertechno_activated()
 {
-        _printf(0, LCD_SEG_L1_3_2, "%02u", it_family);
-        _printf(0, LCD_SEG_L1_1_0, "%02u", it_device);
-        display_symbol(0, LCD_SEG_L1_DP1, SEG_ON);
+    _printf(0, LCD_SEG_L1_3_2, "%02u", it_family);
+    _printf(0, LCD_SEG_L1_1_0, "%02u", it_device);
+    display_symbol(0, LCD_SEG_L1_DP1, SEG_ON);
 }
 
 static void intertechno_deactivated()
 {
-        display_clear(0, 1);
+    display_clear(0, 1);
 }
 
 static void intertechno_up_pressed()
 {
-        rf_tx_cmd(((it_family - 1) << 4) + it_device - 1, INTERTECHNO_CMD_ON);
-        display_chars(0, LCD_SEG_L2_2_0, "ON ", SEG_SET);
+    rf_tx_cmd(((it_family - 1) << 4) + it_device - 1, INTERTECHNO_CMD_ON);
+    display_chars(0, LCD_SEG_L2_2_0, "ON ", SEG_SET);
 }
 
 static void intertechno_down_pressed()
 {
-        rf_tx_cmd(((it_family - 1) << 4) + it_device - 1, INTERTECHNO_CMD_OFF);
-        display_chars(0, LCD_SEG_L2_2_0, "OFF", SEG_SET);
+    rf_tx_cmd(((it_family - 1) << 4) + it_device - 1, INTERTECHNO_CMD_OFF);
+    display_chars(0, LCD_SEG_L2_2_0, "OFF", SEG_SET);
 }
 
 static void intertechno_updown_pressed()
 {
-        rf_tx_cmd(((it_family - 1) << 4) + it_device - 1, INTERTECHNO_CMD_SP);
-        display_chars(0, LCD_SEG_L2_2_0, "SPE", SEG_SET);
+    rf_tx_cmd(((it_family - 1) << 4) + it_device - 1, INTERTECHNO_CMD_SP);
+    display_chars(0, LCD_SEG_L2_2_0, "SPE", SEG_SET);
 }
 
 /*************************** edit mode callbacks **************************/
 static void it_edit_ff_sel(void)
 {
-        display_chars(0, LCD_SEG_L1_3_2, NULL, BLINK_ON);
+    display_chars(0, LCD_SEG_L1_3_2, NULL, BLINK_ON);
 }
 
 static void it_edit_ff_dsel(void)
 {
-        display_chars(0, LCD_SEG_L1_3_2, NULL, BLINK_OFF);
+    display_chars(0, LCD_SEG_L1_3_2, NULL, BLINK_OFF);
 }
 
 static void it_edit_ff_set(int8_t step)
 {
-        helpers_loop(&tmp_family, 1, 16, step);
-        _printf(0, LCD_SEG_L1_3_2, "%02u", tmp_family);
+    helpers_loop(&tmp_family, 1, 16, step);
+    _printf(0, LCD_SEG_L1_3_2, "%02u", tmp_family);
 }
 
 static void it_edit_dd_sel(void)
 {
-        display_chars(0, LCD_SEG_L1_1_0, NULL, BLINK_ON);
+    display_chars(0, LCD_SEG_L1_1_0, NULL, BLINK_ON);
 }
 
 static void it_edit_dd_dsel(void)
 {
-        display_chars(0, LCD_SEG_L1_1_0, NULL, BLINK_OFF);
+    display_chars(0, LCD_SEG_L1_1_0, NULL, BLINK_OFF);
 }
 
 static void it_edit_dd_set(int8_t step)
 {
-        helpers_loop(&tmp_device, 1, 16, step);
-        _printf(0, LCD_SEG_L1_1_0, "%02u", tmp_device);
+    helpers_loop(&tmp_device, 1, 16, step);
+    _printf(0, LCD_SEG_L1_1_0, "%02u", tmp_device);
 }
 
 static void intertechno_save(void)
 {
-        it_family = tmp_family;
-        it_device = tmp_device;
+    it_family = tmp_family;
+    it_device = tmp_device;
 }
 
 /* edit mode item table */
 static struct menu_editmode_item intertechno_items[] = {
-        {&it_edit_dd_sel, &it_edit_dd_dsel, &it_edit_dd_set},
-        {&it_edit_ff_sel, &it_edit_ff_dsel, &it_edit_ff_set},
-        {NULL}
+    {&it_edit_dd_sel, &it_edit_dd_dsel, &it_edit_dd_set},
+    {&it_edit_ff_sel, &it_edit_ff_dsel, &it_edit_ff_set},
+    {NULL}
 };
 
 static void intertechno_num_pressed()
 {
-        menu_editmode_start(&intertechno_save, intertechno_items);
+    menu_editmode_start(&intertechno_save, intertechno_items);
 }
 
 void mod_intertechno_init()
 {
-        menu_add_entry("INTER",
-                       &intertechno_up_pressed,
-                       &intertechno_down_pressed,
-                       &intertechno_num_pressed,
-                       NULL,
-                       NULL, &intertechno_updown_pressed,
-                       &intertechno_activated, &intertechno_deactivated);
+    menu_add_entry("INTER",
+                   &intertechno_up_pressed,
+                   &intertechno_down_pressed,
+                   &intertechno_num_pressed,
+                   NULL,
+                   NULL, &intertechno_updown_pressed,
+                   &intertechno_activated, &intertechno_deactivated);
 }
 
 void rf_init(void)
 {
-        uint8_t PATable[2] = { 0x00, 0xC3 };    //0 dBm
+    uint8_t PATable[2] = { 0x00, 0xC3 };        //0 dBm
 
-        ResetRadioCore();
+    ResetRadioCore();
 
-        // open PMM module registers for write access
-        PMMCTL0_H = 0xA5;
-        // global high power module request enable
-        PMMCTL0_L |= PMMHPMRE;
-        // lock PMM module registers for write access
-        PMMCTL0_H = 0x00;
+    // open PMM module registers for write access
+    PMMCTL0_H = 0xA5;
+    // global high power module request enable
+    PMMCTL0_L |= PMMHPMRE;
+    // lock PMM module registers for write access
+    PMMCTL0_H = 0x00;
 
-        //WriteSingleReg(IOCFG2, 0x29);       //GDO2 Output Configuration
-        //WriteSingleReg(IOCFG1, 0x2E);       //GDO1 Output Configuration
-        WriteSingleReg(IOCFG0, 0x06);   //GDO0 Output Configuration
-        //WriteSingleReg(FIFOTHR, 0x07);      //FIFO thresholds
-        //WriteSingleReg(SYNC1, 0xD3);        //Sync Word, High Byte
-        //WriteSingleReg(SYNC0, 0x91);        //Sync Word, Low Byte
-        WriteSingleReg(PKTLEN, INTERTECHNO_SEQ_SIZE);   //Packet Length
-        //WriteSingleReg(PKTCTRL1, 0x04);     //Packet Automation Control
-        WriteSingleReg(PKTCTRL0, 0x00); //Packet Automation Control
-        //WriteSingleReg(ADDR, 0x00);         //Device Address
-        //WriteSingleReg(CHANNR, 0x00);       //Channel Number
-        //WriteSingleReg(FSCTRL1, 0x0F);      //Frequency Synthesizer Control
-        //WriteSingleReg(FSCTRL0, 0x00);      //Frequency Synthesizer Control
-        WriteSingleReg(FREQ2, 0x10);    //Frequency Control Word, High Byte
-        WriteSingleReg(FREQ1, 0xB0);    //Frequency Control Word, Middle Byte
-        WriteSingleReg(FREQ0, 0x71);    //Frequency Control Word, Low Byte
-        WriteSingleReg(MDMCFG4, 0x86);  //Modem Configuration
-        WriteSingleReg(MDMCFG3, 0x70);  //Modem Configuration
-        WriteSingleReg(MDMCFG2, 0x30);  //Modem Configuration
-        WriteSingleReg(MDMCFG1, 0x02);  //Modem Configuration
-        //WriteSingleReg(MDMCFG0, 0xF8);      //Modem Configuration
-        //WriteSingleReg(DEVIATN, 0x47);      //Modem Deviation Setting
-        //WriteSingleReg(MCSM2, 0x07);        //Main Radio Control State Machine Configuration
-        //WriteSingleReg(MCSM1, 0x30);        //Main Radio Control State Machine Configuration
-        WriteSingleReg(MCSM0, 0x00);      //Main Radio Control State Machine Configuration
-        WriteSingleReg(FOCCFG, 0x76);   //Frequency Offset Compensation Configuration
-        //WriteSingleReg(BSCFG, 0x6C);        //Bit Synchronization Configuration
-        //WriteSingleReg(AGCCTRL2, 0x03);     //AGC Control
-        //WriteSingleReg(AGCCTRL1, 0x40);     //AGC Control
-        //WriteSingleReg(AGCCTRL0, 0x91);     //AGC Control
-        WriteSingleReg(WOREVT1, 0x87);  //High Byte Event0 Timeout
-        WriteSingleReg(WOREVT0, 0x6B);  //Low Byte Event0 Timeout
-        WriteSingleReg(WORCTRL, 0xF8);  //Wake On Radio Control
-        WriteSingleReg(FREND0, 0x11);   //Front End TX Configuration
-        //WriteSingleReg(FSCAL3, 0xE9);       //Frequency Synthesizer Calibration
-        //WriteSingleReg(FSCAL2, 0x2A);       //Frequency Synthesizer Calibration
-        //WriteSingleReg(FSCAL1, 0x00);       //Frequency Synthesizer Calibration
-        //WriteSingleReg(FSCAL0, 0x1F);       //Frequency Synthesizer Calibration
-        //WriteSingleReg(TEST0, 0x09);        //Various Test Settings
-        //WriteSingleReg(LQI, 0x80);  //Demodulator Estimate for Link Quality
-        //WriteSingleReg(LQI, 0xFF);  //Demodulator Estimate for Link Quality
-        //WriteSingleReg(RSSI, 0x80); //Received Signal Strength Indication
-        //WriteSingleReg(MARCSTATE, 0x01);    //Main Radio Control State Machine State
-        //WriteSingleReg(PKTSTATUS, 0x80);    //Current GDOx Status and Packet Status
-        //WriteSingleReg(VCO_VC_DAC, 0xFD);   //Current Setting from PLL Calibration Module
-        //WriteSingleReg(TXBYTES, 0x00);      //Underflow and Number of Bytes
+    // minimal register changes
+    WriteSingleReg(IOCFG0, 0x06);       //GDO0 Output Configuration
+    WriteSingleReg(PKTLEN, INTERTECHNO_SEQ_SIZE);       //Packet Length
+    WriteSingleReg(PKTCTRL0, 0x00);     //Packet Automation Control
+    WriteSingleReg(FREQ2, 0x10);        //Frequency Control Word, High Byte
+    WriteSingleReg(FREQ1, 0xB0);        //Frequency Control Word, Middle Byte
+    WriteSingleReg(FREQ0, 0x71);        //Frequency Control Word, Low Byte
+    WriteSingleReg(MDMCFG4, 0x86);      //Modem Configuration
+    WriteSingleReg(MDMCFG3, 0x70);      //Modem Configuration
+    WriteSingleReg(MDMCFG2, 0x30);      //Modem Configuration
+    WriteSingleReg(MDMCFG1, 0x02);      //Modem Configuration
+    WriteSingleReg(MCSM0, 0x00);        //Main Radio Control State Machine Configuration
+    WriteSingleReg(MCSM1, 0x00);        //Main Radio Control State Machine Configuration
+    WriteSingleReg(FOCCFG, 0x76);       //Frequency Offset Compensation Configuration
+    WriteSingleReg(WOREVT1, 0x87);      //High Byte Event0 Timeout
+    WriteSingleReg(WOREVT0, 0x6B);      //Low Byte Event0 Timeout
+    WriteSingleReg(WORCTRL, 0xF8);      //Wake On Radio Control
+    WriteSingleReg(FREND0, 0x11);       //Front End TX Configuration
 
-        //WriteSingleReg(PA_TABLE1, 0x50);       //0dB output power
-
-        WriteBurstPATable(&PATable[0], 2);
+    WriteBurstPATable(&PATable[0], 2);
 }
 
 uint8_t rotate_byte(uint8_t in)
 {
-        uint8_t rv = 0;
-        rv += (in & 0x10) << 3;
-        rv += (in & 0x20) << 1;
-        rv += (in & 0x40) >> 1;
-        rv += (in & 0x80) >> 3;
-        rv += (in & 0x1) << 3;
-        rv += (in & 0x2) << 1;
-        rv += (in & 0x4) >> 1;
-        rv += (in & 0x8) >> 3;
-        return rv;
+    uint8_t rv = 0;
+    rv += (in & 0x10) << 3;
+    rv += (in & 0x20) << 1;
+    rv += (in & 0x40) >> 1;
+    rv += (in & 0x80) >> 3;
+    rv += (in & 0x1) << 3;
+    rv += (in & 0x2) << 1;
+    rv += (in & 0x4) >> 1;
+    rv += (in & 0x8) >> 3;
+    return rv;
 }
 
 void rf_tx_cmd(uint8_t prefix, uint8_t cmd)
 {
-        uint8_t ib = 0, j;
-        uint8_t rprefix;
-        uint8_t it_buff[INTERTECHNO_SEQ_SIZE];
-        int8_t i;               // unsigned kills it
-        uint16_t int_state;
+    uint8_t ib = 0;
+    uint8_t rprefix;
+    uint8_t it_buff[INTERTECHNO_SEQ_SIZE];
+    int8_t i;
 
-        rprefix = rotate_byte(prefix);
+    rprefix = rotate_byte(prefix);
 
-        for (j = 1; j <= INTERTECHNO_SEQ_PKT; j++) {
-                for (i = 7; i >= 0; i--) {
-                        switch (rprefix & (1 << i)) {
-                        case 0:
-                                it_buff[ib] = 0x88;
-                                break;
-                        default:
-                                it_buff[ib] = 0x8e;
-                                break;
-                        }
-                        ib++;
-                }
-                for (i = 3; i >= 0; i--) {
-                        switch (cmd & (1 << i)) {
-                        case 0:
-                                it_buff[ib] = 0x88;
-                                break;
-                        default:
-                                it_buff[ib] = 0x8e;
-                                break;
-                        }
-                        ib++;
-                }
+    // replace 1 with 0x8e and 0 with 0x88
+    for (i = 7; i >= 0; i--) {
+        if (rprefix & (1 << i)) {
+            it_buff[ib] = 0x8e;
+        } else {
+            it_buff[ib] = 0x88;
+        }
+        ib++;
+    }
 
-                // send sync sequence
-                it_buff[ib] = 0x80;
-                it_buff[ib + 1] = 0;
-                it_buff[ib + 2] = 0;
-                it_buff[ib + 3] = 0;
-                ib += 4;
+    for (i = 3; i >= 0; i--) {
+        if (cmd & (1 << i)) {
+            it_buff[ib] = 0x8e;
+        } else {
+            it_buff[ib] = 0x88;
+        }
+        ib++;
+    }
+
+    // sync sequence
+    it_buff[ib] = 0x80;
+    it_buff[ib + 1] = 0;
+    it_buff[ib + 2] = 0;
+    it_buff[ib + 3] = 0;
+
+    // display RF symbol
+    display_symbol(0, LCD_ICON_BEEPER1, SEG_ON);
+    display_symbol(0, LCD_ICON_BEEPER2, SEG_ON);
+    display_symbol(0, LCD_ICON_BEEPER3, SEG_ON);
+
+    rf_init();
+
+    Strobe(RF_SCAL);            // re-calibrate radio
+
+    // factory remotes send the sequence 4 times
+    for (i = 0; i < 4; i++) {
+        // voodoo
+        //RF1AIES |= BIT9;
+        //RF1AIFG &= ~BIT9;       // Clear pending interrupts
+        //RF1AIE &= ~BIT9;        // Disable TX end-of-packet interrupt
+
+        WriteBurstReg(RF_TXFIFOWR, it_buff, INTERTECHNO_SEQ_SIZE);      // fill up FIFO
+        Strobe(RF_STX);         // TX
+        timer0_delay(60, LPM3_bits);    // each sequence should take at least 54.6 ms
+
+        // for unknown reasons in 90% of cases during the _last_ cycle TXFIFOWR gets filled
+        // but STX doesn't send it
+        // TXFIFO either remains filled forever, until RF_SFTX, or RF_STX - whichever is first
+        // temporary fix
+        if (ReadSingleReg(TXBYTES)) {
+            Strobe(RF_STX);
+            timer0_delay(60, LPM3_bits);
         }
 
-        // display RF symbol
-        display_symbol(0, LCD_ICON_BEEPER1, SEG_ON);
-        display_symbol(0, LCD_ICON_BEEPER2, SEG_ON);
-        display_symbol(0, LCD_ICON_BEEPER3, SEG_ON);
+        while ((Strobe(RF_SNOP) & 0xF0) != 0) ; // wait until radio core ready and IDLE state
+    }
 
-        rf_init();
+    Strobe(RF_SIDLE);           // IDLE
+    Strobe(RF_SFTX);            // flush TXFIFO
+    Strobe(RF_SPWD);            // power-down mode
 
-        ENTER_CRITICAL_SECTION(int_state);
+    // clear RF symbol
+    display_symbol(0, LCD_ICON_BEEPER1, SEG_OFF);
+    display_symbol(0, LCD_ICON_BEEPER2, SEG_OFF);
+    display_symbol(0, LCD_ICON_BEEPER3, SEG_OFF);
 
-        // voodoo zone
-        RF1AIES |= BIT9;
-        RF1AIFG &= ~BIT9;       // Clear pending interrupts
-        RF1AIE &= ~BIT9;        // Disable TX end-of-packet interrupt
-
-        Strobe(RF_SCAL);        // re-calibrate radio
-        timer0_delay(10, LPM3_bits); // probably redundant
-
-        // send twice
-        for (i = 0; i < 2; i++) {
-                WriteBurstReg(RF_TXFIFOWR, it_buff, INTERTECHNO_SEQ_SIZE); // fill up FIFO
-                timer0_delay(10, LPM3_bits); // probably redundant
-                Strobe(RF_STX); // TX
-                timer0_delay(60 * INTERTECHNO_SEQ_PKT, LPM3_bits); // each packet should take at least 54.6 ms
-                while ((Strobe(RF_SNOP) & 0xF0) != 0);
-        }
-
-        Strobe(RF_SIDLE);       // IDLE
-        Strobe(RF_SPWD);        // power-down mode
-
-        EXIT_CRITICAL_SECTION(int_state);
-
-        // clear RF symbol
-        display_symbol(0, LCD_ICON_BEEPER1, SEG_OFF);
-        display_symbol(0, LCD_ICON_BEEPER2, SEG_OFF);
-        display_symbol(0, LCD_ICON_BEEPER3, SEG_OFF);
 }
 
 // *************************************************************************************************
@@ -319,21 +288,21 @@ void rf_tx_cmd(uint8_t prefix, uint8_t cmd)
 // *************************************************************************************************
 void WriteBurstPATable(unsigned char *buffer, unsigned char count)
 {
-        volatile char i = 0;
-        uint16_t int_state;
+    volatile char i = 0;
+    uint16_t int_state;
 
-        ENTER_CRITICAL_SECTION(int_state);
-        while (!(RF1AIFCTL1 & RFINSTRIFG));
-        RF1AINSTRW = 0x7E00 + buffer[i];        // PA Table burst write   
+    ENTER_CRITICAL_SECTION(int_state);
+    while (!(RF1AIFCTL1 & RFINSTRIFG)) ;
+    RF1AINSTRW = 0x7E00 + buffer[i];    // PA Table burst write   
 
-        for (i = 1; i < count; i++) {
-                RF1ADINB = buffer[i];   // Send data
-                while (!(RFDINIFG & RF1AIFCTL1));       // Wait for TX to finish
-        }
-        i = RF1ADOUTB;          // Reset RFDOUTIFG flag which contains status byte
+    for (i = 1; i < count; i++) {
+        RF1ADINB = buffer[i];   // Send data
+        while (!(RFDINIFG & RF1AIFCTL1)) ;      // Wait for TX to finish
+    }
+    i = RF1ADOUTB;              // Reset RFDOUTIFG flag which contains status byte
 
-        while (!(RF1AIFCTL1 & RFINSTRIFG));
-        RF1AINSTRB = RF_SNOP;   // reset PA Table pointer
+    while (!(RF1AIFCTL1 & RFINSTRIFG)) ;
+    RF1AINSTRB = RF_SNOP;       // reset PA Table pointer
 
-        EXIT_CRITICAL_SECTION(int_state);
+    EXIT_CRITICAL_SECTION(int_state);
 }
